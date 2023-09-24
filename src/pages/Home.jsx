b6efbce5cf69de40564e9660d3629374e2eca24f
Selim Ramdani <selimcodeur@gmail.com>
2023-09-24 14:23:05 +0200

    add top homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import Footer from "../components/Footer"
 import Header from "../components/Header"
-
+import homepageBg from "../assets/images/homepage_bg.png";
 function Home() {
 
   return (
     <>
       <Header />
-      <h1>Hello from Home page!</h1>
+      <div id="homepage__top">
+        <img src={homepageBg}  alt="homepage_background" id="homepage__top_bg" />
+        <h1 id="homepage__top_title">Chez vous, partout et ailleurs</h1>
+      </div>
       <Footer />
     </>
   )
