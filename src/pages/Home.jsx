88bfe95a32eecf0f7817e62c9efcaff1b11136fd
Selim Ramdani <selimcodeur@gmail.com>
2023-10-01 08:52:27 +0200

    Home page finished

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,14 @@
-import Footer from "../components/Footer"
 import Header from "../components/Header"
-import homepageBg from "../assets/images/homepage_bg.png";
-function Home() {
+import Banner from "../components/Banner";
+import Gallery from "../components/Gallery";
+import Footer from "../components/Footer"
 
+
+function Home() {
   return (
     <>
-      <Header />
-      <div id="homepage__top">
-        <img src={homepageBg}  alt="homepage_background" id="homepage__top_bg" />
-        <h1 id="homepage__top_title">Chez vous, partout et ailleurs</h1>
-      </div>
-      <Footer />
+      <Banner isAbout={false} />
+      <Gallery />
     </>
   )
 }
