025799b777fe913d6f5725f6b0aac6c66d72b58a
Selim Ramdani <selimcodeur@gmail.com>
2023-10-01 12:22:20 +0200

    Outlet in App.jsx added

--- src/main.jsx
+++ src/main.jsx
@@ -5,28 +5,39 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
+// Style import
+import "./assets/style/main.scss";
+
 // Pages import
 import Accommodations from './pages/Accommodations.jsx';
 import Home from './pages/Home.jsx';
-import Error from './pages/Error.jsx';
 import About from './pages/About.jsx';
+import Header from './components/Header';
+import App from './App';
+import NotFound from './pages/NotFound.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/logements",
-    element: <Accommodations />,
-  },
-  {
-    path: "/a-propos",
-    element: <About />,
-  },
-  {
-    path: "*",
-    element: <Error />,
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/logements/:id",
+        element: <Accommodations />,
+      },
+      {
+        path: "/a-propos",
+        element: <About />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ]
   },
 ]);
 
