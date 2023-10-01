b7e44c70d71bb9f6ab251404310689e68afeebf7
Selim Ramdani <selimcodeur@gmail.com>
2023-10-01 08:42:44 +0200

    Cards component and style finished

---
+++ src/components/Cards.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react"
+import NotFound from "../pages/NotFound";
+import { Link } from "react-router-dom";
+function Cards({ title, cover, id }) {
+
+  return (
+    <Link to={`logements/${id}`}>
+      <div id="cards-container">
+        <img src={cover} alt="cover" id="card-container__img" />
+        <h2 id="card-container__title">{title}</h2>
+      </div>
+    </Link>
+  )
+}
+
+export default Cards
