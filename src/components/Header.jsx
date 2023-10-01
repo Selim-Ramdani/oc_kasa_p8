cd2203a97b25024150dbbf79a404c974bd4232d7
Selim Ramdani <selimcodeur@gmail.com>
2023-10-01 08:43:34 +0200

    Header component and style finished

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,21 +2,21 @@
 import Home from "../pages/Home"
 import About from "../pages/About"
 import { Link, Navigate } from "react-router-dom"
-import logo from "../assets/logo_kasa.png";
+import logo from "../assets/images/logo_kasa.png";
 function Header() {
 
   return (
     <header id="header">
       <div id="header__logo">
-        <img src={logo} alt="logo_kasa" id="logo" />
+        <Link to={"/"}><img src={logo} alt="logo_kasa" id="logo" /></Link>
       </div>
-      <div id="header__links">
-      <Link to="/">
-        <button id="header__links_homepage">Accueil</button>
-      </Link>
-      <Link to="/a-propos">
-        <button id="header__links_aboutpage">A propos</button>
-      </Link>
+      <div id="header-links">
+        <Link to="/">
+          <button id="header-links__homepage">Accueil</button>
+        </Link>
+        <Link to="/a-propos">
+          <button id="header-links__aboutpage">A propos</button>
+        </Link>
       </div>
     </header>
   )
